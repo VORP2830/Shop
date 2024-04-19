d7e838604b6d003bc62825ecdb0ba35b2129ebb6
Victor Osório Ramos Porto <victor.porto@mpse.mp.br>
2024-04-19 07:41:48 -0300

    Aula 220

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
+import 'package:shop/providers/counter.dart';
 import 'package:shop/utils/app_routes.dart';
+
+import 'pages/counter_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,23 +15,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Colors.purple,
-          secondary: Colors.deepOrange,
-          background: Colors.white,
+    return CounterProvider(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.purple,
+            secondary: Colors.deepOrange,
+            background: Colors.white,
+          ),
+          fontFamily: 'Lato',
+          useMaterial3: true,
         ),
-        fontFamily: 'Lato',
-        useMaterial3: true,
+        home: ProductsOverViewPage(),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (_) => CounterPage(),
+        },
+        //Removendo a faixa de debug
+        debugShowCheckedModeBanner: false,
       ),
-      home: ProductsOverViewPage(),
-      routes: {
-        AppRoutes.PRODUCT_DETAIL: (_) => ProductDetailPage(),
-      },
-      //Removendo a faixa de debug
-      debugShowCheckedModeBanner: false,
     );
   }
 }
